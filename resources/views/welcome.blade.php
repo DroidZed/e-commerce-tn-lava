7a1863232cb3f6615f28173be1fe160162193718
DroidZed <41507665+DroidZed@users.noreply.github.com>
2025-05-12 23:03:43 +0100

    thanks to Herd I can work with this

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1784,8 +1784,8 @@
                 @if (Route::has('register'))
                     <a
                         href="{{ route('register') }}"
-                        class="inline-block px-5 py-1.5 dark:text-[#EDEDEC] border-[#19140035] hover:border-[#1915014a] border text-[#1b1b18] dark:border-[#3E3E3A] dark:hover:border-[#62605b] rounded-sm text-sm leading-normal">
-                        Register
+                        class="inline-block px-5 py-1.5 dark:text-[#EDEDEC] border-[#19140035] hover:border-[#1915014a] border text-[#1b1b18] dark:border-[#3E3E3A] dark:hover:border-[#62605b] rounded-sm text-lg leading-normal">
+                        Sign up
                     </a>
                 @endif
             @endauth
